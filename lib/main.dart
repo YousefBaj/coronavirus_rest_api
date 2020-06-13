ae08a193204da89ea2209ad9cace26d238c70a48
yousef <ybajafar@gmail.com>
2020-06-13 13:32:00 +0300

    finish first card in ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:coronavirus_rest_api_flutter_course/app/repositories/data_repositories.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/services/api.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/services/api_service.dart';
+import 'package:coronavirus_rest_api_flutter_course/app/ui/dashboard.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,25 +13,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return Provider<DataRepository>(
+      create: (_) => DataRepository(
+        apiService: APIService(
+          API.sandbox(),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: Color(0xff101010),
+          cardColor: Color(0xff222222),
+        ),
+        home: Dashboard(),
+      ),
     );
   }
 }
@@ -58,12 +56,13 @@ class _MyHomePageState extends State<MyHomePage> {
     final apiService = APIService(API.sandbox());
 
     final accessToken = await apiService.getAccessToken();
-    final result = await apiService.getEndpointData(accessToken: accessToken, endpoint: Endpoints.casesPerOneMillion,);
+    final result = await apiService.getEndpointData(
+      accessToken: accessToken,
+      endpoint: Endpoints.casesPerOneMillion,
+    );
 
     setState(() => _result = result);
   }
-
- 
 
   @override
   Widget build(BuildContext context) {
@@ -78,11 +77,11 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               'You have pushed the button this many times:',
             ),
-            if(_result != null)
-            Text(
-              '$_result',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            if (_result != null)
+              Text(
+                '$_result',
+                style: Theme.of(context).textTheme.headline4,
+              ),
           ],
         ),
       ),
